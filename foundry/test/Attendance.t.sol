3814bafab584b2d0140ffeb93c847f72b88a60de
Raphael Faboyinde <raphaelfaboyinde27@gmail.com>
2026-02-13 18:51:37 +0100

    forgeBuild

---
+++ foundry/test/Attendance.t.sol
@@ -0,0 +1,26 @@
+// // SPDX-License-Identifier: UNLICENSED
+// pragma solidity ^0.8.13;
+
+// import {Test} from "forge-std/Test.sol";
+// import {Attendance} from "../src/Attendance.sol";
+
+// contract AttendanceTest is Test {
+//     Attendance public attendance;
+
+//     function setUp() public {
+//         attendance = new Attendance();
+//     }
+
+//     function test_AddStudent() public {
+//         attendance.addStudent("Alice", 20);
+//         assertEq(attendance.getStudentCount(), 1);
+//     }
+//         counter.increment();
+//         assertEq(counter.number(), 1);
+//     }
+
+//     function testFuzz_SetNumber(uint256 x) public {
+//         counter.setNumber(x);
+//         assertEq(counter.number(), x);
+//     }
+// }
